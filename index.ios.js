f80e285cc255e97bb270bef405d9d7b88aabd711
kkanchamreddy <kiran.kanchamreddy@gmail.com>
2016-05-30 07:43:18 -0700

    fetch real data

--- index.ios.js
+++ index.ios.js
@@ -13,12 +13,66 @@ import {
 	View
 } from 'react-native';
 
-var MOCKED_MOVIES_DATA = [
-	{title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}}
-];
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
 class NativeReact extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			movies: null,
+		};
+	}
+
+	componentDidMount() {
+		this.fetchData();
+	}
+
+	fetchData() {
+		fetch(REQUEST_URL)
+		.then((response) => response.json())
+		.then((responseData) => {
+			this.setState({
+				movies: responseData.movies,
+			});
+		})
+		.done();
+	}
+
 	render() {
-		var movie = MOCKED_MOVIES_DATA[0];
+		if (!this.state.movies) {
+			return this.renderLoadingView();
+		}
+		var movie = this.state.movies[0];
+		return this.renderMovie(movie);
+
+	}
+
+	renderMovie(movie) {
+		return (
+			<View style={styles.container}>
+				<Image
+					source={{uri: movie.posters.thumbnail}}
+					style={styles.thumbnail}
+				/>
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
+      </View>
+    );
+  }
+
+	renderLoadingView() {
+		return (
+			<View style={styles.container}>
+				<Text>
+					Loading movies...
+				</Text>
+			</View>
+		);
+	}
+
+	renderMovie(movie) {
 		return (
 			<View style={styles.container}>
 				<Image
@@ -44,7 +98,7 @@ const styles = StyleSheet.create({
 	},
 	rightContainer: {
 		flex: 1
-  	},
+	  },
 	thumbnail: {
 		width: 53,
 		height: 81
